ee502f0e2801ff6c318f04e4370fbb714101f03e
Salvador Aceves <salvador@aceves.mx>
2021-03-23 22:55:15 -0500

    initial working search commit

---
+++ index.js
@@ -0,0 +1,156 @@
+const axios = require('axios');
+const R = require('ramda');
+require('util').inspect.defaultOptions.depth = null;
+
+const { name: pluginName } = require('./package.json');
+
+const capitalize = (s) => {
+  if (typeof s !== 'string') return '';
+  return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+};
+
+const bookingSearch = ({ client }) => `
+{
+  hotelX {
+    booking(
+      criteria: {
+        accessCode: "0"
+        language: "es"
+        typeSearch: DATES
+        dates: { dateType: BOOKING, start: "2020-01-24", end: "2020-01-25" }
+      }
+      settings: {
+        client: "${client}"
+        auditTransactions: true
+        context: "HOTELTEST"
+        testMode: true
+        timeout: 18000
+      }
+    ) {
+      bookings {
+        billingSupplierCode
+        reference {
+          client
+          supplier
+          hotel
+          bookingID
+        }
+        holder {
+          name
+          surname
+        }
+        status
+        hotel {
+            start
+            end
+          hotelCode
+          hotelName
+          boardCode
+          occupancies {
+            id
+            paxes {
+              age
+            }
+          }
+          rooms {
+            occupancyRefId
+            code
+            description
+            price {
+              currency
+              net
+              exchange {
+                currency
+                rate
+              }
+            }
+          }
+        }
+        cancelPolicy {
+          refundable
+          cancelPenalties {
+            hoursBefore
+            penaltyType
+            currency
+            value
+          }
+        }
+        remarks
+        payable
+      }
+      errors {
+        code
+        type
+        description
+      }
+      warnings {
+        code
+        type
+        description
+      }
+    }
+  }
+}
+
+`;
+
+const doMap = (obj, map) => {
+  const retVal = {};
+  Object.entries(map).forEach(([attribute, fn]) => {
+    const newVal = fn(obj);
+    if (newVal !== undefined) {
+      retVal[attribute] = newVal;
+    }
+  });
+  return retVal;
+};
+
+const bookingMapOut = {
+  id: R.path(['reference', 'bookingID']),
+  status: (e) => capitalize(R.path(['status'], e)),
+  holder: R.path(['holder']),
+  telephone: R.path(['phone']),
+  supplierId: R.path(['reference', 'supplier']),
+  hotelId: R.path(['hotel', 'hotelCode']),
+  hotelName: R.path(['hotel', 'hotelName']),
+  rooms: (e) => e.hotel.rooms.map((r) => ({
+    description: R.path(['description'], r),
+    roomId: R.path(['code'], r), // code
+    price: R.path(['price'], r),
+  })),
+  start: R.path(['hotel', 'start']),
+  end: R.path(['hotel', 'end']),
+};
+
+const getHeaders = (apiKey) => ({
+  Authorization: `ApiKey ${apiKey}`,
+  'Content-Type': 'application/json',
+});
+class Plugin {
+  constructor(params) { // we get the env variables from here
+    Object.entries(params).forEach(([attr, value]) => {
+      const nuName = attr.replace(`${pluginName}-`, '');
+      this[nuName] = value;
+    });
+  }
+
+  async searchHotelBooking({ token: { apiKey, endpoint, client } }) {
+    const url = `${endpoint || this.endpoint}/`;
+    const headers = getHeaders(apiKey || this.apiKey);
+    const data = JSON.stringify({ query: bookingSearch({ client }) });
+    const results = await axios({
+      method: 'post',
+      url,
+      headers,
+      data,
+    });
+    // console.log(results.data);
+    // return doMap(JSON.parse(profile).companyProfile, mapIn);
+    const bookingResult = R.path(['data', 'data', 'hotelX', 'booking'], results);
+    if (bookingResult.errors) throw new Error(bookingResult.error);
+    // console.log(bookingResult.bookings[0]);
+    return { bookings: bookingResult.bookings.map((e) => doMap(e, bookingMapOut)) };
+  }
+}
+
+module.exports = Plugin;
---
+++ index.test.js
@@ -0,0 +1,28 @@
+/* globals describe, beforeAll, it, expect */
+const R = require('ramda');
+const Plugin = require('./index');
+const { name: pluginName } = require('./package.json');
+
+const app = new Plugin(R.pickBy(
+  (_val, key) => key.substring(0, pluginName.length) === pluginName,
+  process.env,
+));
+
+describe('saerch tests', () => {
+  // let validKey = process;
+  beforeAll(async () => {
+    // nada
+  });
+  it(' should be able to search by booking Id', async () => {
+    const retVal = await app.searchHotelBooking({
+      payload: {},
+      token: {
+        apiKey: app.apiKey,
+        endpoint: app.endpoint,
+        client: app.client,
+      },
+    });
+    console.log(retVal.bookings[0]);
+    expect(retVal).toBeTruthy();
+  });
+});
